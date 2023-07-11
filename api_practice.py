7eb1398de5723575fad737578a8aabb61fc913fc
Robby Gavigan <robby@insyte-group.com>
2023-07-11 15:14:50 +0100

    commit 3

--- api_practice.py
+++ api_practice.py
@@ -12,15 +12,6 @@ def get_weather_forecast(latitude, longitude):
         return None
 
 
-# converting all inputs to floats
-latitude = float(input("Enter latitude: "))
-longitude = float(input("Enter longitude: "))
 
-forecast_data = get_weather_forecast(latitude, longitude)
-
-if forecast_data:
-    print(forecast_data)
-else:
-    print("Error occurred while fetching data from the API.")
 
 
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+from api_practice import get_weather_forecast
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharmerino')
+    print("run")
+
+# converting all inputs to floats
+latitude = float(input("Enter latitude: "))
+longitude = float(input("Enter longitude: "))
+
+forecast_data = get_weather_forecast(latitude, longitude)
+
+if forecast_data:
+    print(forecast_data)
+else:
+    print("Error occurred while fetching data from the API.")
