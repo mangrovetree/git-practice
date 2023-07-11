414688c40dbd3f298ffda1d9b4b6ce59a231d862
Robby Gavigan <robby@insyte-group.com>
2023-07-11 14:55:18 +0100

    annotated the code on api prac

--- api_practice.py
+++ api_practice.py
@@ -1,8 +1,10 @@
 import requests
 
 def get_weather_forecast(latitude, longitude):
+    # plugging in parameters to the url
     api_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m"
     response = requests.get(api_url)
+    # if status of api is active, run
     if response.status_code == 200:
         data = response.json()
         return data
@@ -10,6 +12,7 @@ def get_weather_forecast(latitude, longitude):
         return None
 
 
+# converting all inputs to floats
 latitude = float(input("Enter latitude: "))
 longitude = float(input("Enter longitude: "))
 
